3158c73c766f1819193a7b66b00bc974f5de5400
Giovi-17 <giovanni.nuzzo17@gmail.com>
2021-12-17 14:22:36 +0100

    BONUS

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+    <img src="https://redcapes.it/wp-content/uploads/2018/04/ironman-1.jpg" alt="">
+</template>
+
+<script>
+export default {
+    name: "Hero",
+}
+</script>
+
+<style scoped>
+
+    img{
+
+        width: 500px;
+
+    }
+
+</style>
